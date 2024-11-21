a02f81118edb1f712ac99734bb55070985f101a4
Anson <AnsonChung3@gmail.com>
2024-11-21 22:48:43 +0000

    Add stand-alone single file .py project

---
+++ spellchecker.py
@@ -0,0 +1,11 @@
+# This is a pet project, mimicing the demo from CFG Into to Python
+
+from tkinter import *
+from textblob import TextBlob
+
+root = Tk()
+root.title("Hello")
+root.geometry("650x400")
+root.config(background = "#0b5428")
+
+root.mainloop()
