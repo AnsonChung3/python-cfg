2dd3af5048b00a4069f5179125d8b500c0e37788
Anson <AnsonChung3@gmail.com>
2024-11-22 20:35:22 +0000

    Add bare bone widgets to Spell Checker. To add styling. Button not hooked up yet.

--- spellchecker.py
+++ spellchecker.py
@@ -4,8 +4,21 @@ from tkinter import *
 from textblob import TextBlob
 
 root = Tk()
-root.title("Hello")
+root.title("Anson's Pet Project")
 root.geometry("650x400")
 root.config(background = "#0b5428")
+
+header = Label(root, text="Spell Checker")
+header.pack()
+
+secondary_header = Label(root, text="Type a word in the field below andd find out!")
+secondary_header.pack()
+
+input = StringVar()
+text_entry = Entry(root, textvariable=input)
+text_entry.pack()
+
+check_btn = Button(root, text="Check!")
+check_btn.pack()
 
 root.mainloop()
