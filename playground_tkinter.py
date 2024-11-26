c9c17e2551bc1e1ed64a231fcf0518dbd5168c9d
Anson <AnsonChung3@gmail.com>
2024-11-26 21:41:45 +0000

    Added 2 columns of frames. It seems to work, but without colored border it's hard to tell/see.

--- playground_tkinter.py
+++ playground_tkinter.py
@@ -3,4 +3,16 @@ import tkinter as tk
 root = tk.Tk()
 root.title = ("Playground Tkinter!")
 
+sidebar_fr = tk.Frame(root)
+sidebar_fr.grid(row=0, column=0, rowspan=1)
+
+test_lb_left = tk.Label(sidebar_fr, text="Sidebar")
+test_lb_left.grid(row=0, column=0)
+
+main_display_fr = tk.Frame(root)
+main_display_fr.grid(row=0, column=1, rowspan=2)
+
+test_lb_right = tk.Label(main_display_fr, text="Main display")
+test_lb_right.grid(row=0, column=0)
+
 root.mainloop()
